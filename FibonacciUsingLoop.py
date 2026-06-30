22dce2fecd6bf062029df5c6101bf70cdebcf971
Rishita Shah <rishitashah6263@gmail.com>
2026-06-30 20:10:27 +0530

    Fibonacci series output logic
    Fibonacci series calculation to print correct values.

--- FibonacciUsingLoop.py
+++ FibonacciUsingLoop.py
@@ -1,1 +1,15 @@
+n = int(input("Enter the Number: "))
+a = 0
+b = 1
+print("Fabonacci Series")
+for i in range(n):
+  
+  print(i, end=" ")
+  c = a + b
+  a = b
+  b = c
 
+Output: 
+Enter the Number: 10
+Fabonacci Series
+0 1 2 3 4 5 6 7 8 9 
